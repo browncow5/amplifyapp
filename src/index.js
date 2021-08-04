bdc14b29aebeae0272490586e0e7fb19685f3be6
browncow5 <ryan.e.donat@gmail.com>
2021-08-04 11:55:20 +0100

    add source for segment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+<script>
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="45zIITtyEVOCjvq263ECHbEamp5hsd0E";analytics.SNIPPET_VERSION="4.13.2";
+  analytics.load("45zIITtyEVOCjvq263ECHbEamp5hsd0E");
+  analytics.page();
+  }}();
+</script>
+
 Amplify.configure(config);
 
 ReactDOM.render(
